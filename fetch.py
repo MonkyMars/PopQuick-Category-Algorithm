1176979c8a2d9fbde226d20ca7168ba5adf97f97
Monky <levi.laptop@hotmail.com>
2024-12-09 23:17:58 +0100

    Update fetch parameters and enhance error handling; modify feedback data structure to include new categories

--- fetch.py
+++ fetch.py
@@ -1,6 +1,12 @@
 import requests
 
-response = requests.get("http://localhost:5000/api/categories", params={"top_n": 10, "temperature": .1}) # higher temperature = more randomness, and vise versa.
+response = requests.get("http://localhost:5000/api/categories", params={"top_n": 50, "temperature": 0}) # higher temperature = more randomness, and vise versa.
 data = response.json()
-recommendations = data["recommendations"]
-print(recommendations)
+if response.status_code == 200:
+    try:
+        recommendations = data["recommendations"]
+        print(recommendations)
+    except KeyError:
+        print(data["message"], data["status"])
+else:
+    print(data)
